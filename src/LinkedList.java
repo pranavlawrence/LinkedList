16c70f5ecef51c32f62bcf2f2e60af7174e48ccd
pranavlawrence <pranav_aditya@users.noreply.github.com>
2018-09-23 15:35:49 +0530

    Inserting at position of choice

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -48,6 +48,30 @@ public class LinkedList {
         }
         size++;
     }
+    
+    // inserting at position
+    
+    public void insertAtPos(int val, int pos)
+    {
+        if(pos==1)
+            insertAtFirst(val);
+        else if(pos==size+1)
+            insertAtLast(val);
+        else if(pos>1&&pos<=size)
+        {
+            Node n,t;
+            n=new Node(val,null);
+            t=start;
+            for(int i=1;i<pos-1;i++)
+                t=t.getNext();
+            n.setNext(t.getNext());
+            t.setNext(n);
+            size++;
+        }
+        else
+            System.out.println("Insertion at this position not possible");
+        
+    }
             
     
     // Method for checking if the list is empty or not 
